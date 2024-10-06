2cc8896fe332ccd6d19ad14c853e2e31d5c3240c
BrassicaPatch <104414508+BrassicaPatch@users.noreply.github.com>
2024-10-06 03:35:56 -0600

    City Data Json

--- CW - Proof of Concept/util.py
+++ CW - Proof of Concept/util.py
@@ -2,9 +2,11 @@ import json
 from models import *
 
 def load_cities():
-    with open('cities.json', 'r') as file:
+    with open('city_data.json', 'r') as file:
         data = json.load(file)
 
-    cities = [City(item['name'], item['position']) for item in data]
+    #cities = [City(item['name'], item['position']) for item in data]
+
+    cities = [City(item['name'], item['position'], item['population'], item['income'], item['side']) for item in data]
 
     return cities
